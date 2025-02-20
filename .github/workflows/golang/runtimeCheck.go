ebb751997919f6601596937b702f19f21b0ed159
るんく君 <91005690+RunqRun@users.noreply.github.com>
2025-02-20 15:46:13 +0900

    [CREATE] テスト結果の処理をGolangに置換

---
+++ .github/workflows/golang/runtimeCheck.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"io/ioutil"
+	"strings"
+)
+
+func main(){
+	log("main", "Runtime Results Checking...")
+
+	//実行結果の取得
+	const LOG_FILE_PATH = "~/logs/latest.log"
+	data, err := ioutil.ReadFile(LOG_FILE_PATH)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	logContent := string(data)
+
+	//ランタイム実行の結果処理
+	if !compileCheck(logContent) { os.Exit(1) }
+	log("compileCheck", "passed.")
+	if !runtimeCheck(logContent) { os.Exit(1) }
+	log("runtimeCheck", "passed.")
+
+	//終了処理
+	log("main", "Runtime Results are all passed successfully.")
+
+}
+
+func log(logName string, logContent string){
+	fmt.Println("[TheMiningS2] "+logName+": "+logContent)
+}
+
+//コンパイルテスト
+func compileCheck(logContent string) bool{
+	const COMPILE_ERROR_DETECTION_STRING = "[Skript] Line"
+	if strings.Contains(logContent, COMPILE_ERROR_DETECTION_STRING) { return false }
+	return true
+}
+
+//ランタイムテスト
+func runtimeCheck(logContent string) bool{
+	
+	//ランタイムエラー検出
+	const RUNTIME_ERROR_DETECTION_STRING = "THEMININGS2_TEST_FAILED"
+	if strings.Contains(logContent, RUNTIME_ERROR_DETECTION_STRING) { return false }
+
+	return true
+}
