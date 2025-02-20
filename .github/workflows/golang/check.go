c89a68cd434e77e1c35b55dc3fc136ded29b543f
るんく君 <91005690+RunqRun@users.noreply.github.com>
2025-02-20 22:45:37 +0900

    [FIX] ログの発行数の基準を修正

--- .github/workflows/golang/check.go
+++ .github/workflows/golang/check.go
@@ -60,7 +60,7 @@ func runtimeCheck(logContent string) bool{
 	lines := strings.Split(string(data), "\n")
 	fmt.Println(string(data))
 	fmt.Println("DATA QUANTITY: "+string(len(lines)))
-	if len(lines) % AMOUNT_OF_LOG_TYPES != 0 {
+	if strings.Count(string(data), "RUNTIME_TEST_LOG") % AMOUNT_OF_LOG_TYPES != 0 {
 		log("1-LogQuantityModuloCheck", "failed.")
 		os.Exit(1)
 	}
