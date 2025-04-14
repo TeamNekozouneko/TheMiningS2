8394a8967e9c0d2607fa2e1d047ac04b122a81b6
るんく君 <91005690+RunqRun@users.noreply.github.com>
2025-04-14 16:13:58 +0900

    add: 2-SQLiteAvailableCheck

--- .github/workflows/golang/check.go
+++ .github/workflows/golang/check.go
@@ -1,14 +1,14 @@
 package main
 
 import (
-	"fmt"
-	"os"
-	"io/ioutil"
-	"strings"
 	"bufio"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
 )
 
-func main(){
+func main() {
 	log("main", "Runtime Results Checking...")
 
 	//実行結果の取得
@@ -21,9 +21,13 @@ func main(){
 	logContent := string(data)
 
 	//ランタイム実行の結果処理
-	if !compileCheck(logContent) { os.Exit(1) }
+	if !compileCheck(logContent) {
+		os.Exit(1)
+	}
 	log("compileCheck", "passed.")
-	if !runtimeCheck(logContent) { os.Exit(1) }
+	if !runtimeCheck(logContent) {
+		os.Exit(1)
+	}
 	log("runtimeCheck", "passed.")
 
 	//終了処理
@@ -31,31 +35,51 @@ func main(){
 
 }
 
-func log(logName string, logContent string){
-	fmt.Println("[TheMiningS2] "+logName+": "+logContent)
+func log(logName string, logContent string) {
+	fmt.Println("[TheMiningS2] " + logName + ": " + logContent)
 }
 
-//コンパイルテスト
-func compileCheck(logContent string) bool{
+// コンパイルテスト
+func compileCheck(logContent string) bool {
 	const COMPILE_ERROR_DETECTION_STRING = "[Skript] Line"
-	if strings.Contains(logContent, COMPILE_ERROR_DETECTION_STRING) { return false }
+	if strings.Contains(logContent, COMPILE_ERROR_DETECTION_STRING) {
+		return false
+	}
 	return true
 }
 
-//ランタイムテスト
-func runtimeCheck(logContent string) bool{
-	
+// ランタイムテスト
+func runtimeCheck(logContent string) bool {
+
 	//ランタイムエラー検出
 	const RUNTIME_ERROR_DETECTION_STRING = "THEMININGS2_TEST_FAILED"
-	if strings.Contains(logContent, RUNTIME_ERROR_DETECTION_STRING) { return false }
+	if strings.Contains(logContent, RUNTIME_ERROR_DETECTION_STRING) {
+		return false
+	}
 
-	//1-LogQuantityModuloCheck
+	if !LogQuantityModuloCheck(logContent) {
+		os.Exit(1)
+	}
+	if !SQLiteAvailableCheck() {
+		os.Exit(1)
+	}
+
+	return true
+}
+
+func SQLiteAvailableCheck() bool {
+	const DATABASE_FILE_PATH = "plugins/Skript/variables.db"
+	_, err := os.Stat(DATABASE_FILE_PATH)
+	return !os.IsNotExist(err)
+}
+
+func LogQuantityModuloCheck(logContent string) bool {
 	const SYSTEM_LOG_FILE_PATH = "plugins/Skript/logs/themining.log"
 	const AMOUNT_OF_LOG_TYPES = 3
-	
+
 	file, err := os.Open(SYSTEM_LOG_FILE_PATH)
 	if err != nil {
-		os.Exit(1)
+		return false
 	}
 	defer file.Close()
 
@@ -64,14 +88,13 @@ func runtimeCheck(logContent string) bool{
 	for scanner.Scan() {
 		logQuantity += strings.Count(scanner.Text(), "RUNTIME_TEST_LOG")
 	}
-	if logQuantity % AMOUNT_OF_LOG_TYPES != 0 {
+	if logQuantity%AMOUNT_OF_LOG_TYPES != 0 {
 		log("1-LogQuantityModuloCheck", "failed.")
-		os.Exit(1)
+		return false
 	}
 
 	if err := scanner.Err(); err != nil {
-		os.Exit(1)
+		return false
 	}
-
 	return true
 }
